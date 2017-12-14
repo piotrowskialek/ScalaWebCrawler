fb3c301f80c55d5beddf9f2aa2366d45954afc81
piotrowskialek <piotrowskialek@icloud.com>
2017-12-14 23:34:00 +0100

    stem actor

--- src/main/scala/WebCrawler/Stemmer.scala
+++ src/main/scala/WebCrawler/Stemmer.scala
@@ -1,16 +1,19 @@
 package WebCrawler
 
 import akka.actor.Actor
+import morfologik.stemming.polish.PolishStemmer
+
+import scala.collection.JavaConversions._
 
 class Stemmer extends Actor {
+
+  val stemmer = new PolishStemmer
 
   def receive: Receive = {
     case Stem(word: String) =>
       sender() ! StemFinished(word, stem(word))
   }
 
-  def stem(word: String): String = {
-    word
-  }
+  def stem(word: String): String = stemmer.lookup(word).map(wd => wd.getStem.toString).reduce(_ + "/" + _)
 
 }
