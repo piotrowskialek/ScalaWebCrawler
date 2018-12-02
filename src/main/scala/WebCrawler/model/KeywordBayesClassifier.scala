73296610940bf213aacec5bcfa3d55183117c01d
Aleksander Piotrowski <piotrowskialek@icloud.com>
2018-12-02 20:48:19 +0100

    reading csv

--- src/main/scala/WebCrawler/model/KeywordBayesClassifier.scala
+++ src/main/scala/WebCrawler/model/KeywordBayesClassifier.scala
@@ -3,6 +3,7 @@ package WebCrawler.model
 import de.daslaboratorium.machinelearning.classifier.bayes.BayesClassifier
 
 import scala.collection.JavaConverters._
+import scala.io.BufferedSource
 
 class KeywordBayesClassifier() extends KeywordContainerPredicate {
 
@@ -16,20 +17,10 @@ class KeywordBayesClassifier() extends KeywordContainerPredicate {
 object KeywordBayesClassifier {
 
   val bayesClassifier: BayesClassifier[String, Boolean] = new BayesClassifier[String, Boolean]()
-
-  //wczytaj z csv do Map[String, Boolean]
-  val learningMap: Map[String, Boolean] = Map[String, Boolean](
-    "Pogoda na rysy jest kiepska" -> true,
-    "Pozdrawiam mame" -> false,
-    "Jaki masz model zasilacza" -> false,
-    "Na rysach ostatnio była kiepska pogoda" -> true,
-    "Na telefonie mam rysy" -> false,
-    "małe rysy na miarę możliwości naszej jury" -> true,
-    "asewrgertd asdasda sdasdeferg" -> false,
-    "fatalna pogoda opóźniła naszą podróż" -> true
-  )
-  learningMap.foreach(pair => {
-    bayesClassifier.learn(pair._2, pair._1.split("\\s").toList.asJava)
+  val csv: BufferedSource = io.Source.fromFile("src/resources/learning_data.csv")
+  csv.getLines().foreach(rawLine => {
+    val learningData = rawLine.split(",").map(_.trim)
+    val learningWords = learningData(0).split("\\s").toList.asJava
+    bayesClassifier.learn(learningData(1).toBoolean, learningWords)
   })
-
 }
